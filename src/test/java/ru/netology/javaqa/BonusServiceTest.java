3ef2451396b112332b6b27080b95fad2bd2519aa
simonovvanya <ivankartofan@lenta.ru>
2024-11-21 20:54:39 +0300

    Initial commit

---
+++ src/test/java/ru/netology/javaqa/BonusServiceTest.java
@@ -0,0 +1,72 @@
+package ru.netology.javaqa;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class BonusServiceTest {
+
+    @Test
+    void shouldCalculateForRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготовка данных
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
+
+        // вызов целевого метода
+        long actual = service.calculate(amount, registered);
+
+        // сравнение фактического и ожидаемого результата
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготовка данных
+        long amount = 1_000_000;
+        boolean registered = true;
+        long expected = 500;
+
+        // вызов целевого метода
+        long actual = service.calculate(amount, registered);
+
+        // сравнение фактического и ожидаемого результата
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void shouldCalculateForUnregisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготовка данных
+        long amount = 1000;
+        boolean registered = false;
+        long expected = 10;
+
+        // вызов целевого метода
+        long actual = service.calculate(amount, registered);
+
+        // сравнение фактического и ожидаемого результата
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForUnregisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготовка данных
+        long amount = 1_000_000;
+        boolean registered = false;
+        long expected = 500;
+
+        // вызов целевого метода
+        long actual = service.calculate(amount, registered);
+
+        // сравнение фактического и ожидаемого результата
+        Assertions.assertEquals(expected, actual);
+    }
+}
